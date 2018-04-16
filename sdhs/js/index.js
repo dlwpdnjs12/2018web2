52f088178c76ba129146cc50ef0028e6c520479d
17ubi20 <17ubi20@sdh.hs.kr>
2018-04-16 09:45:26 +0900

    section 작업

---
+++ sdhs/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    var i=1;
+    function slide(){
+        $(".slide_box > img").eq(i).animate({left : -1000},0);
+        $(".slide_box > img").eq(i-1).animate({left : 1000},1000);
+        $(".slide_box > img").eq(i).animate({left : 0},1000);
+        i++;
+        if(i>=3){
+            i=0;
+        }
+    }setInterval(slide,3000);
+});
